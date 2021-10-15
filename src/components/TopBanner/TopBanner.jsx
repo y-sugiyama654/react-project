f8ee57b6695f02c69fec1eeba54abc0f5a323b48
Yuta <y.sugiyama54@gmail.com>
2021-10-15 23:39:50 +0900

    Initial Commit

---
+++ src/components/TopBanner/TopBanner.jsx
@@ -0,0 +1,31 @@
+import React, {Component, Fragment} from 'react';
+import { Container } from 'react-bootstrap';
+import '../../asset/css/custom.css';
+import '../../asset/css/bootstrap.min.css';
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "bootstrap/js/src/button";
+
+class TopBanner extends Component {
+    render() {
+        return (
+           <Fragment>
+               <Container fluid={true} className={"topFixedBanner p-0"}>
+                   <div class={"topBannerOverlay"}>
+                       <Container className={"topContent"}>
+                           <Row>
+                               <Col className={"text-center"}>
+                                   <h1 class={"topTitle"}>EASY LEARNING</h1>
+                                   <h4 class={"topSubTitle"}>Learn Professionally</h4>
+                                   {/*<Button variant={"primary"}>Learn More</Button>*/}
+                               </Col>
+                           </Row>
+                       </Container>
+                   </div>
+               </Container>
+           </Fragment>
+        );
+    }
+}
+
+export default TopBanner;
