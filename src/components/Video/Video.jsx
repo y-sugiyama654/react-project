49a92394cc53d017e772eafade07385bf6ae6cdb
Yuta <y.sugiyama54@gmail.com>
2021-11-07 17:05:06 +0900

    Video Componetの実装2

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -1,7 +1,22 @@
 import React, { Component, Fragment } from 'react'
-import { Col, Container, Row } from 'react-bootstrap'
+import { Col, Container, Modal, Row,Button } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faVideoSlash} from '@fortawesome/free-solid-svg-icons'
+import 'video-react/dist/video-react.css'
+import { Player, BigPlayButton } from 'video-react'
 
 class Video extends Component {
+
+    constructor(){
+        super();
+        this.state={
+            show:false
+        }
+    }
+
+    modalClose=()=>this.setState({show:false})
+    modalOpen=()=>this.setState({show:true})
+
     render() {
         return (
             <Fragment>
@@ -9,7 +24,7 @@ class Video extends Component {
                     <h1 className="serviceMainTitle">OUR VIDEOS</h1>
                     <div className="bottom"></div>
                     <Row>
-                        <Col lg={6} md={6} sm={12}>
+                        <Col lg={6} md={6} sm={12} className="videText">
                             <p className="serviceDescription text-justify">
                                 Hi! I'm Kazi Ariyan. I'm a web developer with a serious love for teaching I am a founder of eLe easy Learning and a passionate Web Developer, Programmer & Instructor.<br></br><br></br>
 
@@ -18,12 +33,23 @@ class Video extends Component {
                                 Each course has been hand-tailored to teach a specific skill. I hope you agree! Whether you’re trying to learn a new skill from scratch or want to refresh your memory on something you’ve learned in the past, you’ve come to the right place.
                             </p>
                         </Col>
-
-                        <Col lg={6} md={6} sm={12}>
-                            <h1>Video</h1>
+                        <Col lg={6} md={6} sm={12} className="videoCard">
+                            <FontAwesomeIcon onClick={this.modalOpen}  className="iconProject" icon={faVideoSlash} />
                         </Col>
                     </Row>
                 </Container>
+                <Modal size="lg" show={this.state.show} onHide={this.modalClose}>
+                    <Modal.Body>
+                        <Player src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4">
+                            <BigPlayButton position="center" />
+                        </Player>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={this.modalClose}>
+                            Close
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
             </Fragment>
         )
     }
