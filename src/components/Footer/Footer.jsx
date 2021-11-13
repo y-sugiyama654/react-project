1e216c2a99f0e6492d93539ea841dc12f4813a4c
Yuta <y.sugiyama54@gmail.com>
2021-11-13 23:38:28 +0900

    Footer Componentの作成2

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,6 +4,8 @@ import { Col, Row, Container } from 'react-bootstrap'
 import {faFacebook} from '@fortawesome/free-brands-svg-icons'
 import {faYoutube} from '@fortawesome/free-brands-svg-icons'
 import {faTwitter} from '@fortawesome/free-brands-svg-icons'
+import {faEnvelope} from '@fortawesome/free-solid-svg-icons'
+import {faPhone} from '@fortawesome/free-solid-svg-icons'
 
 class Footer extends Component {
     render() {
@@ -11,32 +13,46 @@ class Footer extends Component {
             <Fragment>
                 <Container fluid={true} className="footerSection">
                     <Row>
-                        <Col lg="3" md={6} sm={12}>
+                        <Col lg="3" md={6} sm={12} className="p-5 text-center">
                             <h2 className="footerName text-center">Follow Us </h2>
 
-                            <a href="#">
-                                <FontAwesomeIcon icon={faFacebook} size="2x" />
-                            </a>
-                            <a href="#">
-                                <FontAwesomeIcon icon={faYoutube} size="2x" />
-                            </a>
-                            <a href="#">
-                                <FontAwesomeIcon icon={faTwitter} size="2x" />
-                            </a>
+                            <div className="social-container">
+                                <a className="facebook social" href="#">
+                                    <FontAwesomeIcon icon={faFacebook} size="2x" />
+                                </a>
+                                <a href="#"  className="youtube social">
+                                    <FontAwesomeIcon icon={faYoutube} size="2x" />
+                                </a>
+                                <a href="#"  className="twitter social">
+                                    <FontAwesomeIcon icon={faTwitter} size="2x" />
+                                </a>
+                            </div>
                         </Col>
-                        <Col lg="3" md={6} sm={12}>
-                            <h2 className="footerName text-center">Address </h2>
+                        <Col lg="3" md={6} sm={12} className="p-5 text-justify">
+                            <h2 className="footerName">Address </h2>
+                            <p className="footerDescription">
+                                6522 Baltimore National Pike CatonsVille, Califonia USA <br></br>
+                                <FontAwesomeIcon icon={faEnvelope}  /> Email : Support@ele.com<br></br>
+                                <FontAwesomeIcon icon={faPhone}  /> Phone : 434343434<br></br>
+                            </p>
                         </Col>
-                        <Col lg="3" md={6} sm={12}>
-                            <h2 className="footerName text-center">Information </h2>
+                        <Col lg="3" md={6} sm={12} className="p-5 text-justify">
+                            <h2 className="footerName">Information </h2>
+                            <a className="footerLink" href="#">About Me </a> <br></br>
+                            <a  className="footerLink" href="#">Company Profile </a> <br></br>
+                            <a className="footerLink" href="#">Contact Us  </a> <br></br>
                         </Col>
-
-                        <Col lg="3" md={6} sm={12}>
-                            <h2 className="footerName text-center">Policy  </h2>
+                        <Col lg="3" md={6} sm={12} className="p-5 text-justify">
+                            <h2 className="footerName">Policy  </h2>
+                            <a className="footerLink" href="#">Fefund Policy  </a> <br></br>
+                            <a className="footerLink" href="#">Trems And Condition  </a> <br></br>
+                            <a className="footerLink" href="#">Privaci Policy   </a> <br></br>
                         </Col>
                     </Row>
                 </Container>
-
+                <Container fluid={true} className="text-center copyrightSection">
+                    <a className="copyrightlink" href="#">© Copyright 2016 by easy Learning, All Rights Reserved</a>
+                </Container>
             </Fragment>
         )
     }
--- src/components/TopNavigation/TopNavigation.jsx
+++ src/components/TopNavigation/TopNavigation.jsx
@@ -12,6 +12,7 @@ class TopNavigation extends Component {
         this.state={
             navBarTitle:"navTitle",
             navBarLogo: [whiteLogo], // object
+            navVariant:"dark",
             navBarBack:"navBackground",
             navBarItem:"navItem"
         }
@@ -19,9 +20,11 @@ class TopNavigation extends Component {
 
     onScroll=()=>{
         if(window.scrollY>100){
-            this.setState({navBarTitle:'navTitleScroll',navBarLogo:[blackLogo],navBarBack:'navBackgroundScroll',navBarItem:'navItemScroll'})
+            this.setState({navBarTitle:'navTitleScroll',navBarLogo:[blackLogo],navBarBack:'navBackgroundScroll',navBarItem:'navItemScroll',navVariant:'light'})
+
         }else if(window.scrollY<100){
-            this.setState({navBarTitle:'navTitle',navBarLogo:[whiteLogo],navBarBack:'navBackground',navBarItem:'navItem'})
+
+            this.setState({navBarTitle:'navTitle',navBarLogo:[whiteLogo],navBarBack:'navBackground',navBarItem:'navItem',navVariant:'dark'})
         }
     }
 
@@ -33,14 +36,14 @@ class TopNavigation extends Component {
         return (
             <Fragment>
 
-                <Navbar className={this.state.navBarBack} collapseOnSelect fixed="top" expand="lg" variant="dark">
+                <Navbar className={this.state.navBarBack} collapseOnSelect fixed="top" expand="lg" variant={this.state.navVariant}>
                     <Navbar.Brand className={this.state.navBarTitle} href="#home"> <img src={this.state.navBarLogo} /> </Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="mr-auto">
 
                         </Nav>
-                        <Nav className={"nav-bar-item"}>
+                        <Nav>
                             <Nav.Link className={this.state.navBarItem} href="#deets">HOME</Nav.Link>
                             <Nav.Link className={this.state.navBarItem} href="#deets">ABOUT</Nav.Link>
                             <Nav.Link className={this.state.navBarItem}  href="#deets">SERVICE</Nav.Link>
